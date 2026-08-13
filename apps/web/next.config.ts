c64f406dbda4685383b64665eafb852d0101fa50
963sup <963sup@gmail.com>
2026-08-13 22:35:26 +0800

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['127.0.0.1'],
   poweredByHeader: false,
   transpilePackages: [
     '@no-code-collaboration-platform/application',
